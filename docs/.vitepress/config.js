e04a1d9e837c4573135040dd4767c99fa2fb6dad
danielenriquez59 <deenriquez92@gmail.com>
2023-01-28 14:17:21 -0800

    Initial commit of My First Portfolio

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+// .vitepress/config.js
+export default {
+    title: 'Adocs',
+    description: 'An awesome docs template built by me',
+    themeConfig: {
+        siteTitle: "Adocs",
+      },
+    nav: [
+        { text: "About", link: "/about" },
+        { text: "Contact", link: "/contact" },
+        { text: "Guide", link: "/guide" },
+        { text: "Configs", link: "/configs" },
+        { text: "Changelog", link: "https://github.com/..." },
+      ],
+  };
