507ab780defa2bb33d0853be8f74722b79de8d48
Eric Uldall <ericuldall@gmail.com>
2023-01-29 10:39:27 -0800

    updated with working navbar and sass

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,11 @@
 export default {
   title: 'Testing',
-  description: 'Just playing around.'
+  description: 'Just playing around.',
+  themeConfig: {
+    nav: [
+      { text: 'About Me', link: '/about' },
+      { text: 'Page 1', link: '/page1' },
+      { text: 'Page 2', link: '/page2' }
+    ]
+  }
 }
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,5 +2,5 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.scss'
 
 export default {
-	...DefaultTheme,
+	...DefaultTheme
 }
