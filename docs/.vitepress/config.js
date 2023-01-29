b74a55bff840a5e62d106fea3fab31f0b2b52cec
danielenriquez59 <deenriquez92@gmail.com>
2023-01-29 10:26:20 -0800

    working

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,4 @@
-// .vitepress/config.js
 export default {
-    title: 'Adocs',
-    description: 'An awesome docs template built by me',
-    themeConfig: {
-        siteTitle: "Adocs",
-      },
-    nav: [
-        { text: "About", link: "/about" },
-        { text: "Contact", link: "/contact" },
-        { text: "Guide", link: "/guide" },
-        { text: "Configs", link: "/configs" },
-        { text: "Changelog", link: "https://github.com/..." },
-      ],
-  };
+  title: 'Testing',
+  description: 'Just playing around.'
+}
